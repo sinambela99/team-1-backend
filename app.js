0388b82e2ac924858f113338ce1ce6e42cbd8706
arifbp98 <arifbudianto62@yahoo.co.id>
2023-06-26 14:27:01 +0700

    minor update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
+const formidableMiddleware = require("express-formidable");
 dotenv.config();
 const port = process.env.PORT;
 const errorHandler = require("./middlewares/errHandler");
@@ -9,6 +10,7 @@ const errorHandler = require("./middlewares/errHandler");
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(formidableMiddleware())
 app.use("/api", require("./routes/user.routes"));
 app.use("/api", require("./routes/product.routes"));
 app.use("/api", require("./routes/product_detail.routes"));
@@ -16,6 +18,7 @@ app.use("/api", require("./routes/stock.routes"));
 app.use("/api", require("./routes/payment_transaction.routes"));
 app.use("/api", require("./routes/cart.routes"));
 app.use("/api", require("./routes/category.routes"));
+app.use("/api", require("./routes/payment_transaction_detail.controller"));
 app.use(errorHandler);
 
 app.get("/", (req, res) => {
