610cd434b396a8732e9c55af2b64fd614ba6c1f9
CrocoXXX <lawrencenoman@gmail.com>
2023-06-29 04:46:23 +0700

    add info and fix product_detail controller

--- controllers/product_detail.controller.js
+++ controllers/product_detail.controller.js
@@ -1,9 +1,10 @@
 const { Product_detail } = require("../models");
 
 class Controller {
+  // Get All Product Details
   static async getAllProduct_detail(req, res, next) {
     try {
-      const result = await Product_detail.findAll();
+      const result = await Product_detail.findAll({ order: [["id", "ASC"]] });
 
       res.status(200).json({ data: result });
     } catch (err) {
@@ -11,22 +12,7 @@ class Controller {
     }
   }
 
-  static async newProduct_detail(req, res, next) {
-    try {
-      const { ProductId, StockId, CategoryId } = req.fields;
-
-      const product_detail = await Product_detail.create({
-        ProductId,
-        StockId,
-        CategoryId,
-      });
-
-      res.status(200).json({ message: `New Product_detail with id ${product_detail.id} created.` });
-    } catch (err) {
-      next(err);
-    }
-  }
-
+  // Get Product Details By Id
   static async getProduct_detailById(req, res, next) {
     try {
       const result = await Product_detail.findByPk(req.params.id);
@@ -41,18 +27,52 @@ class Controller {
     }
   }
 
-  static async updateProduct_detail(req, res, next) {
+  // Create Product Detail
+  static async newProduct_detail(req, res, next) {
     try {
-      const { ProductId, StockId, CategoryId } = req.body;
+      const { ProductId, StockId, CategoryId } = req.fields;
 
-      const product_detail = await Product_detail.update({ ProductId, StockId, CategoryId }, { where: { id: req.params.id } });
+      const product_detail = await Product_detail.create({
+        ProductId,
+        StockId,
+        CategoryId,
+      });
 
-      res.status(200).json({ message: "Product_detail updated" });
+      res.status(200).json({
+        message: `New Product_detail with id ${product_detail.id} created.`,
+      });
     } catch (err) {
       next(err);
     }
   }
 
+  // Update Product Detail
+  static async updateProduct_detail(req, res, next) {
+    try {
+      const product_detail = await Product_detail.findByPk(req.params.id);
+
+      if (!product_detail) {
+        throw { name: "NotFound" };
+      }
+
+      const { ProductId, StockId, CategoryId } = req.body;
+
+      Product_detail.update(
+        { ProductId, StockId, CategoryId },
+        { where: { id: req.params.id } }
+      );
+
+      res
+        .status(200)
+        .json({
+          message: `Product_detail with id ${product_detail.id} updated`,
+        });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  // Delete Product Detail
   static async deleteProduct_detail(req, res, next) {
     try {
       const product_detail = await Product_detail.findByPk(req.params.id);
@@ -63,7 +83,9 @@ class Controller {
 
       Product_detail.destroy({ where: { id: req.params.id } });
 
-      res.status(200).json({ message: `Product_detail with id ${product_detail.id} deleted` });
+      res.status(200).json({
+        message: `Product_detail with id ${product_detail.id} deleted`,
+      });
     } catch (err) {
       next(err);
     }
