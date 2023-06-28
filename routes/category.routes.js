67cbd6cc88986c40f746281fe9500107a86637de
CrocoXXX <lawrencenoman@gmail.com>
2023-06-29 02:53:05 +0700

    update category routes

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const Controller = require('../controllers/category.controller')
+const express = require("express");
+const router = express.Router();
+const Controller = require("../controllers/category.controller");
 
-router.get('/category',Controller.getAllCategory)
-router.get('/category/:id',Controller.getCategoryById)
-router.post('/category', Controller.newCategory)
-router.put('/category/:id', Controller.updateCategory)
-router.delete('/category/:id', Controller.deleteCategory)
+router.get("/category", Controller.getAllCategory);
+router.get("/category/:id", Controller.getCategoryById);
+router.post("/category", Controller.createNewCategory);
+router.put("/category/:id", Controller.updateCategory);
+router.delete("/category/:id", Controller.deleteCategory);
 
-module.exports = router
+module.exports = router;
