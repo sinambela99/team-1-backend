0b32f4e5cc42c2d5a60014b9e50aa74e78e509f4
Satrio Hutomo <strhtmo17@gmail.com>
2023-06-28 16:29:40 +0700

    revisi app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
+const routes = require("./routes");
 const formidableMiddleware = require("express-formidable");
 dotenv.config();
 const port = process.env.PORT;
@@ -11,19 +12,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(formidableMiddleware());
-app.use("/api", require("./routes/user.routes"));
-app.use("/api", require("./routes/product.routes"));
-app.use("/api", require("./routes/product_detail.routes"));
-app.use("/api", require("./routes/stock.routes"));
-app.use("/api", require("./routes/payment_transaction.routes"));
-app.use("/api", require("./routes/cart.routes"));
-app.use("/api", require("./routes/category.routes"));
-app.use("/api", require("./routes/payment_transaction_detail.routes"));
+app.use(routes);
 app.use(errorHandler);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
