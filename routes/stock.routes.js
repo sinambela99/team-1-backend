c95473ade685790b6af06c35b1a49475327eb0f8
Satrio Hutomo <strhtmo17@gmail.com>
2023-06-30 20:15:59 +0700

    add and fixing stock

--- routes/stock.routes.js
+++ routes/stock.routes.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const Controller = require('../controllers/stock.controller')
+const express = require("express");
+const router = express.Router();
+const Controller = require("../controllers/stock.controller");
 
-router.get('/stocks',Controller.getAllStocks)
-router.get('/stocks/:id',Controller.getStockById)
-router.post('/stocks', Controller.createNewProduct)
-router.put('/stocks/:id', Controller.updateStock)
-router.delete('/stocks/:id', Controller.deleteStock)
+router.get("/", Controller.getAllStocks);
+router.get("/:id", Controller.getStockById);
+router.post("/", Controller.createNewProduct);
+router.put("/:id", Controller.updateStock);
+router.delete("/:id", Controller.deleteStock);
 
-module.exports = router
+module.exports = router;
