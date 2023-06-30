31003152d5ef34c1173f3d83b6a03e09d6ac2e1b
Satrio Hutomo <strhtmo17@gmail.com>
2023-06-30 20:16:21 +0700

    add and fixing payment transaction detail

--- routes/payment_transaction_detail.routes.js
+++ routes/payment_transaction_detail.routes.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const Controller = require('../controllers/payment_transaction_detail.controller')
+const express = require("express");
+const router = express.Router();
+const Controller = require("../controllers/payment_transaction_detail.controller");
 
-router.get('/payment-detail',Controller.getAllPayTransDet)
-router.get('/payment-detail/:id',Controller.getPayTransDetById)
-router.post('/payment-detail', Controller.createNewPayTransDet)
-router.put('/payment-detail/:id', Controller.updatePayTransDet)
-router.delete('/payment-detail/:id', Controller.deletePayTransDet)
+router.get("/", Controller.getAllPayTransDet);
+router.get("/:id", Controller.getPayTransDetById);
+router.post("/", Controller.createNewPayTransDet);
+router.put("/:id", Controller.updatePayTransDet);
+router.delete("/:id", Controller.deletePayTransDet);
 
-module.exports = router
+module.exports = router;
